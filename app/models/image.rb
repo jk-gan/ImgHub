437f7a770fb1323fe6ea65a426abe52e87c881ad
Gan Jun Kai <kuhn96@gmail.com>
2017-04-23 18:00:44 +0800

    Add validation for image

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,12 @@ class Image < ApplicationRecord
 
   has_attached_file :image, styles: { show: '300x300#' },
     default_url: "missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates_attachment :image, presence: true, content_type: { content_type: /\Aimage\/.*\z/ }
+  # validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   process_in_background :image
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
 end
