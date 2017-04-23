5216fca6ee5a15c071af6769e8521effc1a0ab8c
Gan Jun Kai <kuhn96@gmail.com>
2017-04-23 17:27:22 +0800

    Add delay paperclip

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'paperclip', git: 'git://github.com/thoughtbot/paperclip.git'
 gem 'simple_form'
 gem 'jquery-rails'
 gem 'pundit'
+gem 'delayed_paperclip'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,9 @@
 class Image < ApplicationRecord
   belongs_to :user
 
-  has_attached_file :image, styles: { show: '300x300#', medium: "300x300>", thumb: "100x100>" },
-    default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { show: '300x300#' },
+    default_url: "missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  process_in_background :image
 end
