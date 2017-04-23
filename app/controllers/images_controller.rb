62ef8745719f64a848824d722ce86ff1318e560b
Gan Jun Kai <kuhn96@gmail.com>
2017-04-23 12:53:02 +0800

    User can upload image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,11 +3,35 @@ class ImagesController < ApplicationController
   end
 
   def new
+    @image = current_user.images.new
+  end
+
+  def create
+    @image = current_user.images.new(image_params)
+    if @image.save
+      flash[:success] = "Image uploaded successfully"
+      redirect_to @image
+    else
+      Rails.logger.info "Error #{@image.errors.full_messages}"
+      flash[:error] = "Failed to upload image"
+      render :new
+    end
   end
 
   def edit
   end
 
   def show
+    @image = Image.find(params[:id])
+  end
+
+  private
+
+  def image_params
+  	params.require(:image).permit(
+  		:name,
+  		:description,
+  		:image
+  	)
   end
 end
